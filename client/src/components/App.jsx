bdf1cbf7910c987148ccca1a6c79e7c090d9a7a6
Vincent <vincentcolano@gmail.com>
2023-08-10 16:32:28 -0500

    Overview skeleton setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import getProducts from '../../fetch.jsx';
+import Overview from './Overview/Overview.jsx';
 
 const App = () => {
   return (
-    <div> Bye World </div>
+    <div>
+      <div>
+        <Overview />
+      </div>
+      Hello World
+    </div>
   );
 };
 
