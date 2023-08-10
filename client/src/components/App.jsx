3ff7fa42a749f0eb51703124c50d62e5032204c6
mcuellar98 <mcuellar228@gmail.com>
2023-08-10 18:56:45 -0400

    Added css file in source

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import RatingsReviews from './R&R/R&R.jsx'
+import RatingsReviews from './R&R/R&R.jsx';
 import getProducts from '../../fetch.jsx';
 import QA from './QA/QA.jsx';
 import Overview from './Overview/Overview.jsx';
