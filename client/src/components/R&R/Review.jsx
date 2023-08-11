11a59b45e4f722ff0d93bd35e8f3d7d23c5cbc46
Daniel <dnlprk7777@gmail.com>
2023-08-11 14:26:39 -0400

    stars and partially filled stars

--- client/src/components/R&R/Review.jsx
+++ client/src/components/R&R/Review.jsx
@@ -1,19 +1,45 @@
 const Review = ( { review } ) => {
   console.log(review.rating)
 
-  const stars = (rating) => {
+  const filledStars = (rating) => {
     const filled = Math.floor(rating);
-    const part = Math.round(10 * (3.6 % 1)) / 10;
+    var result = '';
+    for (var i = 0; i < filled; i++) {
+      result += '★'
+    }
+    return (<span id='filled'>{result}</span>)
+  }
+  const partFilled = (rating) => {
+    if (rating % 1 !== 0) {
+      const part = Math.round(10 * (3.6 % 1)) / 10;
+      const roundPart = Math.round(part * 4) / 4;
+      var result = '★';
+      const el = document.getElementById('partFilled');
+      if (roundPart === 0.25) {
+        el.style.background = 'linear-gradient(to right, white 25%, transparent 25%, transparent 100%)';
+      } else if (roundPart === 0.5) {
+        el.style.background = 'linear-gradient(to right, white 50%, transparent 25%, transparent 100%)';
+      } else if (roundPart === 0.75) {
+        el.style.background = 'linear-gradient(to right, white 75%, transparent 25%, transparent 100%)';
+      }
+      return result;
+    } else {
+      return;
+    }
   }
 
+  const empty = (rating) => {
+    var result = ''
+    for (var i = 0; i < (5 - Math.ceil(rating)); i++) {
+      result += '☆';
+    }
+    return (<span>{result}</span>)
+  }
   return (
     <div id='tile'>
       <div id='rnd'>
         <div id='stars'>
-          <div id='stars-filled'>
-          <span>★{stars(review.rating)}</span>
-          </div>
-          <span>☆</span>
+          <span>{filledStars(review.rating)}<span id='partFilled'>{partFilled(review.rating)}</span>{empty(review.rating)}</span>
         </div>
         <span>{review.reviewer_name}</span>
       </div>
