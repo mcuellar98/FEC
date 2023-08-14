d85e9f3512b1d7007bdc43e30bbc9aa204c4e1c8
mcuellar98 <michael_cuellar@alumni.brown.edu>
2023-08-14 17:15:57 -0400

    Added files for forms for adding questions and answers

---
+++ client/src/components/QA/AddQuestion.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddQuestion = () => {
+  return (
+    <form className='add_question_modal'> Ask Your Question
+
+    </form>
+  );
+};
+
+export default AddQuestion;
