f0b9b1739077dc2660d8f885415280a99efab94e
Daniel <dnlprk7777@gmail.com>
2023-08-14 01:23:03 -0400

    fixed partial ratings

--- client/src/components/R&R/R&R.jsx
+++ client/src/components/R&R/R&R.jsx
@@ -17,6 +17,7 @@ const RatingsReviews = (props) => {
     get(props.id);
   },[])
 
+//#region fetch stuff
   var get = (prodID) => {
     getReviewsById(prodID)
       .then((res) => {
@@ -40,6 +41,66 @@ const RatingsReviews = (props) => {
         console.log(err)
       })
   }
+//#endregion
+
+  //#region stars
+
+  //refactor to have % of stars in one function
+  //.05,0.1,.15 -> 5
+  const partFilled = (rating) => {
+    const part = Math.round(20 * (rating / 5)) / 20;
+    console.log(part);
+    var styles = {
+      color: 'transparent',
+      WebkitBackgroundClip: 'text',
+      fontSize: '16px',
+      WebkitTextStroke: '0.7px white',
+    }
+    if (part === 0.05) {
+      styles.backgroundImage = 'linear-gradient(to right, white 7.5%, transparent 7.5%, transparent 100%)';
+    } else if (part === 0.1) {
+      styles.backgroundImage = 'linear-gradient(to right, white 10%, transparent 10%, transparent 100%)';
+    } else if (part === 0.15) {
+      styles.backgroundImage = 'linear-gradient(to right, white 12.5%, transparent 12.5%, transparent 100%)';
+    } else if (part === 0.2) {
+      styles.backgroundImage = 'linear-gradient(to right, white 20%, transparent 20%, transparent 100%)';
+    } else if (part === 0.25) {
+      styles.backgroundImage = 'linear-gradient(to right, white 27.5%, transparent 27.5%, transparent 100%)';
+    } else if (part === 0.3) {
+      styles.backgroundImage = 'linear-gradient(to right, white 30%, transparent 30%, transparent 100%)';
+    } else if (part === 0.35) {
+      styles.backgroundImage = 'linear-gradient(to right, white 32.5%, transparent 32.5%, transparent 100%)';
+    } else if (part === 0.4) {
+      styles.backgroundImage = 'linear-gradient(to right, white 40%, transparent 40%, transparent 100%)';
+    } else if (part === 0.45) {
+      styles.backgroundImage = 'linear-gradient(to right, white 47.5%, transparent 47.5%, transparent 100%)';
+    } else if (part === 0.5) {
+      styles.backgroundImage = 'linear-gradient(to right, white 50%, transparent 50%, transparent 100%)';
+    } else if (part === 0.55) {
+      styles.backgroundImage = 'linear-gradient(to right, white 52.5%, transparent 52.5%, transparent 100%)';
+    } else if (part === 0.6) {
+      styles.backgroundImage = 'linear-gradient(to right, white 60%, transparent 60%, transparent 100%)';
+    } else if (part === 0.65) {
+      styles.backgroundImage = 'linear-gradient(to right, white 67.5%, transparent 67.5%, transparent 100%)';
+    } else if (part === 0.7) {
+      styles.backgroundImage = 'linear-gradient(to right, white 70%, transparent 70%, transparent 100%)';
+    } else if (part === 0.75) {
+      styles.backgroundImage = 'linear-gradient(to right, white 72.5%, transparent 72.5%, transparent 100%)';
+    } else if (part === 0.8) {
+      styles.backgroundImage = 'linear-gradient(to right, white 80%, transparent 80%, transparent 100%)';
+    } else if (part === 0.85) {
+      styles.backgroundImage = 'linear-gradient(to right, white 87.5%, transparent 87.5%, transparent 100%)';
+    } else if (part === 0.9) {
+      styles.backgroundImage = 'linear-gradient(to right, white 90%, transparent 90%, transparent 100%)';
+    } else if (part === 0.95) {
+      styles.backgroundImage = 'linear-gradient(to right, white 92.5%, transparent 92.5%, transparent 100%)';
+    } else if (part === 1) {
+      styles.backgroundImage = 'linear-gradient(to right, white 100%, transparent 100%, transparent 100%)';
+    }
+    return (<span id='partFilled' style={styles} >★★★★★</span>);
+  }
+
+  //#endregion
 
   return (
     <div>
@@ -47,14 +108,14 @@ const RatingsReviews = (props) => {
       <div id='RnR-par'>
         <div id='RnR'>
           <div id='ratings'>
-            <RatingBreakdown id={props.id}/>
+            <RatingBreakdown id={props.id} reviews={data} partFilled={partFilled} />
             <ProductBreakdown id={props.id}/>
           </div>
           <div id='space-between'></div>
           <div id='reviews-sec'>
             <div id='reviews'>
               <Sorter id={props.id} reviews={data}/>
-              <ReviewList id={props.id} reviews={data}/>
+              <ReviewList id={props.id} reviews={data} partFilled={partFilled} />
             </div>
             <div id='add-more-container'>
               <MoreReviews id={props.id}/>
--- client/src/components/R&R/RatingBreakdown.jsx
+++ client/src/components/R&R/RatingBreakdown.jsx
@@ -1,7 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
 const RatingBreakdown = (props) => {
+  const [ avg,setAvg ] = useState(0);
+
+  const average = (reviews) => {
+    var sum = 0;
+    for (var i = 0; i < reviews.length; i++) {
+      sum += reviews[i].rating;
+    }
+    return Math.floor(10 * (sum / reviews.length)) / 10;
+  }
+
+  useEffect(() => {
+    setAvg(average(props.reviews));
+  },[props.reviews]);
+
   return (
     <div>
-      Rating Breakdown
+      Rating Breakdown {avg}
+      <div id='stars'>
+          <span>{props.partFilled(avg)}</span>
+        </div>
     </div>
   )
 }
