7f0efcf2e8b7e79fc296bff2251e07e5c9135b26
Daniel <dnlprk7777@gmail.com>
2023-08-14 11:02:05 -0400

    refactored partFilled equation

--- client/src/components/R&R/R&R.jsx
+++ client/src/components/R&R/R&R.jsx
@@ -49,53 +49,21 @@ const RatingsReviews = (props) => {
   //.05,0.1,.15 -> 5
   const partFilled = (rating) => {
     const part = Math.round(20 * (rating / 5)) / 20;
-    console.log(part);
+    const percent = 100 * part;
     var styles = {
       color: 'transparent',
       WebkitBackgroundClip: 'text',
       fontSize: '16px',
       WebkitTextStroke: '0.7px white',
     }
-    if (part === 0.05) {
-      styles.backgroundImage = 'linear-gradient(to right, white 7.5%, transparent 7.5%, transparent 100%)';
-    } else if (part === 0.1) {
-      styles.backgroundImage = 'linear-gradient(to right, white 10%, transparent 10%, transparent 100%)';
-    } else if (part === 0.15) {
-      styles.backgroundImage = 'linear-gradient(to right, white 12.5%, transparent 12.5%, transparent 100%)';
-    } else if (part === 0.2) {
-      styles.backgroundImage = 'linear-gradient(to right, white 20%, transparent 20%, transparent 100%)';
-    } else if (part === 0.25) {
-      styles.backgroundImage = 'linear-gradient(to right, white 27.5%, transparent 27.5%, transparent 100%)';
-    } else if (part === 0.3) {
-      styles.backgroundImage = 'linear-gradient(to right, white 30%, transparent 30%, transparent 100%)';
-    } else if (part === 0.35) {
-      styles.backgroundImage = 'linear-gradient(to right, white 32.5%, transparent 32.5%, transparent 100%)';
-    } else if (part === 0.4) {
-      styles.backgroundImage = 'linear-gradient(to right, white 40%, transparent 40%, transparent 100%)';
-    } else if (part === 0.45) {
-      styles.backgroundImage = 'linear-gradient(to right, white 47.5%, transparent 47.5%, transparent 100%)';
-    } else if (part === 0.5) {
-      styles.backgroundImage = 'linear-gradient(to right, white 50%, transparent 50%, transparent 100%)';
-    } else if (part === 0.55) {
-      styles.backgroundImage = 'linear-gradient(to right, white 52.5%, transparent 52.5%, transparent 100%)';
-    } else if (part === 0.6) {
-      styles.backgroundImage = 'linear-gradient(to right, white 60%, transparent 60%, transparent 100%)';
-    } else if (part === 0.65) {
-      styles.backgroundImage = 'linear-gradient(to right, white 67.5%, transparent 67.5%, transparent 100%)';
-    } else if (part === 0.7) {
-      styles.backgroundImage = 'linear-gradient(to right, white 70%, transparent 70%, transparent 100%)';
-    } else if (part === 0.75) {
-      styles.backgroundImage = 'linear-gradient(to right, white 72.5%, transparent 72.5%, transparent 100%)';
-    } else if (part === 0.8) {
-      styles.backgroundImage = 'linear-gradient(to right, white 80%, transparent 80%, transparent 100%)';
-    } else if (part === 0.85) {
-      styles.backgroundImage = 'linear-gradient(to right, white 87.5%, transparent 87.5%, transparent 100%)';
-    } else if (part === 0.9) {
-      styles.backgroundImage = 'linear-gradient(to right, white 90%, transparent 90%, transparent 100%)';
-    } else if (part === 0.95) {
-      styles.backgroundImage = 'linear-gradient(to right, white 92.5%, transparent 92.5%, transparent 100%)';
-    } else if (part === 1) {
-      styles.backgroundImage = 'linear-gradient(to right, white 100%, transparent 100%, transparent 100%)';
+    if (percent % 10 === 0) {
+      styles.backgroundImage = `linear-gradient(to right, white ${percent}%, transparent ${percent}%, transparent 100%)`;
+    } else if ((percent - 5) % 20 === 0) {
+      const temp = percent + 2.5;
+      styles.backgroundImage = `linear-gradient(to right, white ${temp}%, transparent ${temp}%, transparent 100%)`;
+    } else {
+      const temp = percent - 2.5;
+      styles.backgroundImage = `linear-gradient(to right, white ${temp}%, transparent ${temp}%, transparent 100%)`;
     }
     return (<span id='partFilled' style={styles} >★★★★★</span>);
   }
