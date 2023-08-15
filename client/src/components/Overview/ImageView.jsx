4b4671df54ae4c738fe5243d6dc37da8f567efe1
Vincent <vincentcolano@gmail.com>
2023-08-15 13:14:55 -0500

    image carousel

--- client/src/components/Overview/ImageView.jsx
+++ client/src/components/Overview/ImageView.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState, useEffect } from 'react';
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 const ImageView = (props) => {
+  const [currentPhoto, setCurrentPhoto] = useState(0);
 
+  const imgStyle = {
+  };
+
+  useEffect(() => {
+
+  }, []);
+
+  console.log(props.images.photos[0].url);
   return (
     <div id='images'>
-      Images
+      <Carousel className='carousel-photo' useKeyboardArrows>
+        {props.images.photos.map((p, i) =>
+          (
+            <div key={i}>
+              <img style={imgStyle} src={p.url} />
+              <p className="photo-name">{props.images.name}</p>
+            </div>
+          )
+        )}
+      </Carousel>
     </div>
   );
 };
