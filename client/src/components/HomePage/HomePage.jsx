dc28bfc33c0becbdb921f93022dc62bc4b6de66b
Daniel <dnlprk7777@gmail.com>
2023-08-23 15:17:35 -0400

    some changes

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -21,6 +21,7 @@ const HomePage = ({set}) => {
   const handleClick = (e,value) => {
     e.preventDefault();
     setPageN(value);
+
   }
   const goNext = (e) => {
     setPageN(pageNum + 1);
@@ -37,12 +38,12 @@ const HomePage = ({set}) => {
       </div>
       <div id='pages'>
         {(pageNum === 1) ?
-        (<u onClick={goNext}>&#60;</u>) : (<></>)}
-        <u onClick={(e)=>{handleClick(e,1)}}>1</u>
-        <u onClick={(e)=>{handleClick(e,2)}}>2</u>
-        <u onClick={(e)=>{handleClick(e,3)}}>3</u>
-        <u onClick={(e)=>{handleClick(e,4)}}>4</u>
-        <u onClick={(e)=>{handleClick(e,5)}}>5</u>
+        (<></>) : (<u onClick={goNext}>&#60;</u>) }
+        <u id='page1' onClick={(e)=>{handleClick(e,1)}}>1</u>
+        <u id='page2' onClick={(e)=>{handleClick(e,2)}}>2</u>
+        <u id='page3' onClick={(e)=>{handleClick(e,3)}}>3</u>
+        <u id='page4' onClick={(e)=>{handleClick(e,4)}}>4</u>
+        <u id='page5' onClick={(e)=>{handleClick(e,5)}}>5</u>
         {(pageNum === 5) ?
         (<></>):(<u onClick={goBack}>&#62;</u>)}
       </div>
