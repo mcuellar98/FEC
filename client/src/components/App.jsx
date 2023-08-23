2ec31af07a88f5c46ae28053dd51de49d1552ebd
Daniel <dnlprk7777@gmail.com>
2023-08-23 18:46:58 -0400

    fixed pageNum error

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
       <QA product_id={productId}/>
       <RatingsReviews id={productId} setProductRating={setProductRating}/>
        </div>) :
-      (<HomePage pageNum={pageNum} set={handleClick}/>)
+      (<HomePage set={handleClick}/>)
       }
     </div>
   );
